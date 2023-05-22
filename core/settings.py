b4dc7bed5d7413b88c6cf04e0d5eb2e8c3e058d0
Toktorov <ktoktorov2005@gmail.com>
2023-05-22 08:36:46 +0600

    static config

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'root'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
